e57db231324cad29de52f6e77707431f3d9d896e
Shawn Tapps <stapps.prsvr@gmail.com>
2024-05-20 09:50:47 -0500

    structure changes

--- day8.js
+++ day8.js
@@ -1,2 +1,19 @@
 // 5-16-24
 
+//objects - rule the world
+function reverseString(input) {
+    // console.log(input)
+    let result = []       //empty array to hold values from loop
+    let split = input.split("")    // .split(") converts a string to a array
+    // console.log("split", split)
+    for (let i = 0; i < split.length; i++) {
+        // console.log("i:", i, "split[i]", split[i])
+                result.unshift(split[i])                         //unshift() puts item in front of array
+        // console.log("result", result, "split[i]", split[i])
+    }
+// console.log("result", result.join(""))
+return result.join("")    // .join("")  converts the array back to a string
+}
+console.log(reverseString("abcdefghijklmnopqrstuvwxyz"))
+
+//need to change string structure
---
+++ day9.js
@@ -0,0 +1,12 @@
+//5-20-24
+
+// dom manipulation -- see other computer dev folder for cheatsheets
+
+// html=>   <script src="script.js" defer></script>
+
+//properties in dev tools in broswer.....displays dom
+
+//  let el = document.getElementById("text");
+//el.innerHTML = "Dom is Pokemon!"
+//el.style.color = "red"
+// console.log(el)
